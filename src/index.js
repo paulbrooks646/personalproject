b7fa2440af80be6be0059b819fea512b22e232cd
paulbrooks646 <paulbrooks646@gmail.com>
2021-02-01 15:32:48 -0700

    hashrouter

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {HashRouter, BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import store from './ducks/store.js';
-const Router = process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <Router>
+    <HashRouter>
     <App />
-    </Router>
+    </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

887026e86fe42e4fcb46f052f6db8f717f318800
paulbrooks646 <paulbrooks646@gmail.com>
2021-02-02 10:02:53 -0700

    browser router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import store from './ducks/store.js';
@@ -11,9 +11,9 @@ import store from './ducks/store.js';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <HashRouter>
+    <Router>
     <App />
-    </HashRouter>
+    </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

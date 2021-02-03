9c12002bc20c2c88852fc326c1dd289d1c6506f4
paulbrooks646 <paulbrooks646@gmail.com>
2021-02-03 15:27:40 -0700

    lalala

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
 
 const app = express()
 
-app.use(express.static(__dirname + "/../build"))
+// app.use(express.static(__dirname + "/../build"))
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../build/index.html"))
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"))
+})
 
 
 app.use(express.json())

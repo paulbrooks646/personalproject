e236e431e7733754ee414c88f682cdeec05a6355
paulbrooks646 <paulbrooks646@gmail.com>
2021-02-03 15:33:07 -0700

    hosting

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require("path")
 
 const app = express()
 
-// app.use(express.static(__dirname + "/../build"))
+app.use(express.static(__dirname + "/../build"))
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../build/index.html"))

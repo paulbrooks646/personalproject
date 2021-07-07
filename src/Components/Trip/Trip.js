dab80850885afa8447c29437ce529dfaac6c71d2
paulbrooks646 <paulbrooks646@gmail.com>
2021-07-07 16:34:32 -0600

    trip cards

--- src/Components/Trip/Trip.js
+++ src/Components/Trip/Trip.js
@@ -4,9 +4,19 @@ import { getAttractions } from "../../ducks/attractionReducer";
 import axios from "axios";
 import "./Trip.scss";
 import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 function Trip(props) {
   const [trip, setTrip] = useState([]);
+  const [fantasyCard, setFantasyCard] = useState(false)
+  const [mainCard, setMainCard] = useState(false)
+  const [frontierCard, setFrontierCard] = useState(false)
+  const [critterCard, setCritterCard] = useState(false)
+  const [newCard, setNewCard] = useState(false)
+  const [adventureCard, setAdventureCard] = useState(false)
+  const [tomorrowCard, setTomorrowCard] = useState(false)
+  const [toonCard, setToonCard] = useState(false)
 
   useEffect(() => {
     axios.get("/api/attractions").then((res) => {
@@ -35,9 +45,13 @@ function Trip(props) {
   const tripArray = trip.map((e, index) => (
     <div className="currenttrip" key={index}>
       <h2 className={`trip${e.name}`}>{e.name}</h2>
-      <button onClick={() => removeFromTrip(e.event_id)}>
-        Remove From Trip
-      </button>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => removeFromTrip(e.event_id)}
+      >
+        REMOVE
+      </Button>
     </div>
   ));
 
@@ -47,7 +61,9 @@ function Trip(props) {
         <Link to={`/Attraction/${e.attraction_id}`}>
           <h2 className={`trip${e.name}`}>{e.name}</h2>
         </Link>
-        <button
+        <Button
+          variant="contained"
+          color="primary"
           onClick={() =>
             newEvent(
               props.match.params.trip_id,
@@ -56,8 +72,8 @@ function Trip(props) {
             )
           }
         >
-          Add to Trip
-        </button>
+          Add
+        </Button>
       </div>
     );
   });
@@ -65,15 +81,17 @@ function Trip(props) {
     <>
       <div className="tripmain">
         <div className="trip">
-          {attractionsArray}
-          <h2 className="tripfantasy">Fantasyland</h2>
-          <h2 className="tripmains">Main Street USA</h2>
-          <h2 className="tripfrontier">Frontierland</h2>
-          <h2 className="tripcritter">Critter Country</h2>
-          <h2 className="tripnew">New Orleans Square</h2>
-          <h2 className="tripadventure">Adventureland</h2>
-          <h2 className="triptomorrow">Tomorrowland</h2>
-          <h2 className="triptoon">Toon Town</h2>
+          {/* {attractionsArray} */}
+          <Typography variant="h2" className="tripfantasy">
+            Fantasyland
+          </Typography>
+          <Typography variant="h2" className="tripmains">Main Street USA</Typography>
+          <Typography variant="h2" className="tripfrontier">Frontierland</Typography>
+          <Typography variant="h2" className="tripcritter">Critter Country</Typography>
+          <Typography variant="h2" className="tripnew">New Orleans Square</Typography>
+          <Typography variant="h2" className="tripadventure">Adventureland</Typography>
+          <Typography variant="h2" className="triptomorrow">Tomorrowland</Typography>
+          <Typography variant="h2" className="triptoon">Toon Town</Typography>
         </div>
         <div className="triptrip">
           <h2 className="tripyourday">Your day:</h2>
